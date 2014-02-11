98837e4e587a3b4525de631501b9b330b2623085
Mickaël Raulet <mraulet@insa-rennes.fr>
2014-02-12 00:51:03 +0100

    hevc: add idct sse4 intrinsics funtions

---
+++ libavcodec/x86/hevcdsp.h
@@ -0,0 +1,29 @@
+#ifndef AVCODEC_X86_HEVCDSP_H
+#define AVCODEC_X86_HEVCDSP_H
+
+struct SAOParams;
+struct AVFrame;
+struct UpsamplInf;
+struct HEVCWindow;
+
+// #define OPTI_ASM
+
+//IDCT functions
+
+void ff_hevc_transform_skip_8_sse(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+
+
+void ff_hevc_transform_4x4_luma_add_8_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_4x4_luma_add_10_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+
+void ff_hevc_transform_4x4_add_8_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_4x4_add_10_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_8x8_add_8_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_8x8_add_10_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_16x16_add_8_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_16x16_add_10_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_32x32_add_8_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+void ff_hevc_transform_32x32_add_10_sse4(uint8_t *_dst, int16_t *coeffs, ptrdiff_t _stride);
+
+
+#endif // AVCODEC_X86_HEVCDSP_H
---
+++ libavcodec/x86/hevcdsp_init.c
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2013 Seppo Tomperi
+ *
+ * This file is part of ffmpeg.
+ *
+ * ffmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ffmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ffmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "config.h"
+#include "libavutil/cpu.h"
+#include "libavutil/x86/asm.h"
+#include "libavutil/x86/cpu.h"
+#include "libavcodec/get_bits.h" /* required for hevcdsp.h GetBitContext */
+#include "libavcodec/hevcdsp.h"
+#include "libavcodec/x86/hevcdsp.h"
+
+/***********************************/
+
+#define MCQ_FUNC(DIR, DEPTH, OPT)                                        \
+    void ff_put_hevc_mc_pixels_ ## DIR ## _ ## DEPTH ## _ ## OPT(int16_t *dst, ptrdiff_t dststride, uint8_t *_src, ptrdiff_t _srcstride, int width, int height, int mx, int my);
+
+#define MCQ_FUNCS(type, depth) \
+   MCQ_FUNC( 2, depth, sse4)    \
+   MCQ_FUNC( 4, depth, sse4)    \
+   MCQ_FUNC( 8, depth, sse4)    \
+   MCQ_FUNC(16, depth, sse4)
+
+MCQ_FUNCS(uint8_t,   8)
+
+/***********************************/
+/* deblocking */
+
+#define LFC_FUNC(DIR, DEPTH, OPT)                                        \
+void ff_hevc_ ## DIR ## _loop_filter_chroma_ ## DEPTH ## _ ## OPT(uint8_t *_pix, ptrdiff_t _stride, int *_tc, uint8_t *_no_p, uint8_t *_no_q);
+
+#define LFL_FUNC(DIR, DEPTH, OPT)                                        \
+void ff_hevc_ ## DIR ## _loop_filter_luma_ ## DEPTH ## _ ## OPT(uint8_t *_pix, ptrdiff_t stride, int *_beta, int *_tc, \
+uint8_t *_no_p, uint8_t *_no_q);
+
+#define LFC_FUNCS(type, depth) \
+LFC_FUNC(h, depth, sse2)  \
+LFC_FUNC(v, depth, sse2)
+
+#define LFL_FUNCS(type, depth) \
+LFL_FUNC(h, depth, ssse3)  \
+LFL_FUNC(v, depth, ssse3)
+
+
+LFC_FUNCS(uint8_t,   8)
+LFL_FUNCS(uint8_t,   8)
+
+//LF_FUNCS(uint16_t, 10)
+
+
+void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
+{
+    int mm_flags = av_get_cpu_flags();
+
+    if (bit_depth == 8) {
+        if (EXTERNAL_MMX(mm_flags)) {
+            /*if (mm_flags & AV_CPU_FLAG_CMOV)
+                c->h264_luma_dc_dequant_idct = ff_h264_luma_dc_dequant_idct_mmx; */
+
+            if (EXTERNAL_MMXEXT(mm_flags)) {
+#if ARCH_X86_32 && HAVE_MMXEXT_EXTERNAL
+                /* MMEXT optimizations */
+#endif /* ARCH_X86_32 && HAVE_MMXEXT_EXTERNAL */
+
+                if (EXTERNAL_SSE2(mm_flags)) {
+                }
+                if (EXTERNAL_SSSE3(mm_flags)) {
+
+//                    c->transform_4x4_luma_add = ff_hevc_transform_4x4_luma_add_8_sse4;
+//                    c->transform_add[0] = ff_hevc_transform_4x4_add_8_sse4;
+//                    c->transform_add[1] = ff_hevc_transform_8x8_add_8_sse4;
+                    c->transform_add[2] = ff_hevc_transform_16x16_add_8_sse4;
+                    c->transform_add[3] = ff_hevc_transform_32x32_add_8_sse4;
+
+                }
+                if (EXTERNAL_SSE4(mm_flags)) {
+
+
+                    c->transform_skip     = ff_hevc_transform_skip_8_sse;
+
+                }
+                if (EXTERNAL_AVX(mm_flags)) {
+                }
+            }
+        }
+    } else if (bit_depth == 10) {
+        if (EXTERNAL_MMX(mm_flags)) {
+            if (EXTERNAL_MMXEXT(mm_flags)) {
+#if ARCH_X86_32
+#endif /* ARCH_X86_32 */
+                if (EXTERNAL_SSE2(mm_flags)) {
+#if HAVE_ALIGNED_STACK
+                    /*stuff that requires aligned stack */
+#endif /* HAVE_ALIGNED_STACK */
+                }
+                if (EXTERNAL_SSE4(mm_flags)) {
+                    c->transform_4x4_luma_add = ff_hevc_transform_4x4_luma_add_10_sse4;
+
+                    c->transform_add[0] = ff_hevc_transform_4x4_add_10_sse4;
+                    c->transform_add[1] = ff_hevc_transform_8x8_add_10_sse4;
+                    c->transform_add[2] = ff_hevc_transform_16x16_add_10_sse4;
+                    c->transform_add[3] = ff_hevc_transform_32x32_add_10_sse4;
+                }
+                if (EXTERNAL_AVX(mm_flags)) {
+                }
+            }
+        }
+    }
+}
