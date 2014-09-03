970a66dd3f1a302fa34821e2df2769fcf5df35e7
plepere <pierre-edouard.lepere@insa-rennes.fr>
2014-09-03 13:06:47 +0200

    adds SSE & AVX2 filter_band function @TODO : change base filter_band function for set width ?

--- libavcodec/x86/hevcdsp.h
+++ libavcodec/x86/hevcdsp.h
@@ -306,6 +306,26 @@ WEIGHTING_PROTOTYPES(12, sse4);
 ///////////////////////////////////////////////////////////////////////////////
 // SAO functions
 ///////////////////////////////////////////////////////////////////////////////
+
+void ff_hevc_sao_band_filter_0_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,
+        struct SAOParams *sao, int *borders, int width, int height, int c_idx);
+
+void ff_hevc_sao_band_filter_0_64_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_56_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_32_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_16_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_8_8_sse2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+
+
+void ff_hevc_sao_band_filter_0_8_avx2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,
+        struct SAOParams *sao, int *borders, int width, int height, int c_idx);
+
+void ff_hevc_sao_band_filter_0_32_8_avx2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_56_8_avx2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+void ff_hevc_sao_band_filter_0_64_8_avx2( uint8_t *_dst, uint8_t *_src, ptrdiff_t _stride_dst, ptrdiff_t _stride_src,int16_t* sao_val, int8_t sao_left, int height);
+
+
+
 //#ifndef OPTI_ASM
 void ff_hevc_sao_edge_filter_0_8_sse(uint8_t *_dst, uint8_t *_src,
                                      ptrdiff_t _stride_dst, ptrdiff_t _stride_src, struct SAOParams *sao,
