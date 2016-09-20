4a0a822a26e67a7ecd5a128128f0f630f0fed4f4
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2016-09-20 11:03:31 +0200

    Comment out unused function print_md5() in hevc.C

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -4099,12 +4099,12 @@ fail:
     return ret;
 }
 
-static void print_md5(void *log_ctx, int level, uint8_t md5[16])
-{
-    int i;
-    for (i = 0; i < 16; i++)
-        av_log(log_ctx, level, "%02"PRIx8, md5[i]);
-}
+//static void print_md5(void *log_ctx, int level, uint8_t md5[16])
+//{
+//    int i;
+//    for (i = 0; i < 16; i++)
+//        av_log(log_ctx, level, "%02"PRIx8, md5[i]);
+//}
 
 //static int verify_md5(HEVCContext *s, AVFrame *frame)
 //{
